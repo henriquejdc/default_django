3b83696fc89af8a69bee4ffb1764708a818db3f9
mascDriver <diogoblibi@hotmail.com>
2020-01-21 17:58:28 -0300

    inicializacao dos arquivos de config da autenticacao do user

--- default_project/settings.py
+++ default_project/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'default_project.shop'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+AUTH_USER_MODEL = 'shop.user.CustomUser'

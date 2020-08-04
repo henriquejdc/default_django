495ee36924f5e40f6bab457f5ac05dad3ab7ca25
mascDriver <diogoblibi@hotmail.com>
2020-08-03 21:50:35 -0300

    adicionando ao carrinho com sucesso

--- applications/shop/views/shopping_cart.py
+++ applications/shop/views/shopping_cart.py
@@ -5,10 +5,7 @@ from applications.product.models import Sku
 
 def shopping_cart(request, template='shop/checkout/shopping_cart.html'):
     ctx = {}
-    if ShoppingCart.objects.filter(user__user_id=request.user.id).exists():
-        ctx['carrinho'] = ShoppingCart.objects.get(user=request.user)
-    elif ShoppingTemporaryCart.objects.filter(session=request.session).exists():
-        ctx['carrinho'] = ShoppingCart.objects.get(session=request.session)
+    ctx['carrinho'] = request.session['cart']
 
     return render(request, template, ctx)
 
@@ -21,14 +18,11 @@ def add_to_cart(request, id):
     sku = Sku.objects.get(id=id)
     items.sku = sku
     items.save()
-    product_document = {
-        'title': sku.name,
-        'price': sku.price.price
+    product = {
+        'sku': sku,
+        'quantity': 0
     }
-    try:
-        request.session['cart'][str(sku.id)] = product_document
-    except:
-        request.session['cart'] = {}
-
-    print(request.session['cart'])
+    lista = request.session['cart']
+    lista.append(sku)
+    request.session['cart'] = lista
     return render(request, template_name='index.html')
